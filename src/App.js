28dded707b15e5d6c85826bf2042ccfd1e2e7a0e
reyoucat <re.you.cat@gmail.com>
2021-05-03 00:37:26 +0900

    Chore: 빈 프로젝트 기초 설정

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import './App.css';
+import React from 'react';
+
+function App() {
+  let config = {
+    'test': 123456,
+  };
+
+  console.log(config.test);
+  return (
+    <div className="App">
+      <header className="App-header">
+        BLANK PAGE
+      </header>
+    </div>
+  );
+}
+
+export default App;
