33aae0218d38768027225ac1ecbc402ed353d173
ipflegen <ipflegen@pm.me>
2021-05-05 18:53:37 +0900

    Add: ImageButton test UI

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import React from 'react';
+import ImageButton from './components/ImageButton';
 
 function App() {
   let config = {
@@ -8,10 +9,20 @@ function App() {
 
   console.log(config.test);
   return (
-    <div className="App">
-      <header className="App-header">
-        BLANK PAGE
-      </header>
+    <div className="App App-header">
+      <div className="row-layout">
+        <ImageButton src="./image/us.png" currency="USD" />
+        <ImageButton src="./image/swap.png"/>
+        <ImageButton src="./image/ko.png" currency="KRW" />
+      </div>
+      <div className="row-layout">
+        <ImageButton src="./image/us.png" currency="USD" />
+        <ImageButton src="./image/ko.png" currency="KRW" />
+        <ImageButton src="./image/eu.png" currency="EUR" />
+        <ImageButton src="./image/ja.png" currency="JPY" />
+        <ImageButton src="./image/india.png" currency="INR" />
+        <ImageButton src="./image/thai.png" currency="THB" />
+      </div>
     </div>
   );
 }
