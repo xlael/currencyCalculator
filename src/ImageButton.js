71240385f55909f7c79c1487ef602f3dc969d93f
hayeon17kim <hayeon17kim@gmail.com>
2021-05-07 03:25:33 +0900

    Add: ImageButton component

---
+++ src/ImageButton.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import './ImageButton.css';
+
+function ImageButton(props) {
+
+  const { name, text, className } = props;
+
+  return (
+    <div className='image-button'>
+      <img 
+        src={require(`./images/${ name }.png`).default} 
+        alt={ name } 
+        className={ className }/>
+      <div>{ text }</div>
+    </div>
+  );
+}
+
+ImageButton.propTypes = {
+  name: PropTypes.string,
+  text: PropTypes.string,
+  className: PropTypes.string
+};
+
+export default ImageButton;
--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
@@ -5,7 +6,7 @@ import App from './App';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <App/>
   </React.StrictMode>,
   document.getElementById('root')
 );
