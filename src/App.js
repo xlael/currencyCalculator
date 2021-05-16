4773f5a04ee5536d03862bad5b5c3d419149e0d6
Hayeon Kim <hayeon17kim@gmail.com>
2021-05-16 21:14:46 +0900

    Feature/hayeon image button (#4)
    PR을 머지 합니다.
    
    * Chore: 빈 프로젝트 기초 설정 (#1)
    
    @ipflegen  @hayeon17kim  리뷰 감사합니다. 머지하겠습니다.
    
    * Add: ImageButton component
    
    * Fix: move images directory to public
    
    Co-authored-by: reyoucat <re.you.cat@gmail.com>

--- src/App.js
+++ src/App.js
@@ -1,18 +1,56 @@
 import './App.css';
 import React from 'react';
+import ImageButton from './ImageButton.js';
+
+const korea = {
+  name: 'south-korea',
+  className: 'big-flag',
+  text: '대한민국 KRW',
+};
+
+const japan = {
+  name: 'japan',
+  className: 'big-flag',
+  text: '일본 JPY',
+};
+
+const unitedStates = {
+  name: 'united-states',
+  className: 'small-flag',
+  text: '미국 USD',
+};
+
+const mexico = {
+  name: 'mexico',
+  className: 'small-flag',
+  text: '멕시코 MXN',
+};
+
+const switchButton = {
+  name: 'switch',
+  className: 'switch',
+};
+
 
 function App() {
-  let config = {
-    'test': 123456,
-  };
 
-  console.log(config.test);
   return (
-    <div className="App">
-      <header className="App-header">
-        BLANK PAGE
-      </header>
-    </div>
+    <>
+      <div>
+        <h2>Big Flag Image Button</h2>
+        <ImageButton {...korea} />
+        <ImageButton {...japan} />
+      </div>
+      <div>
+        <h2>Small Flag Image Button</h2>
+        <ImageButton {...unitedStates} />
+        <ImageButton {...mexico}/>
+      </div>
+      <div>
+        <h2>Switch Image Button</h2>
+        <ImageButton {...switchButton} />
+      </div>
+    </>
   );
 }
 
