5b3fcd858585077449897e156d83bb3219230464
sangheon-kim <ksj83671@gmail.com>
2021-06-06 22:53:37 +0900

    [김상헌] 2차 개편안 제공

---
+++ src/App2.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import './App.css';
+import ImageButton from './ImageButton.js';
+import country from './assets/dummy.json';
+
+const switchButton = {
+  name: 'switch',
+  className: 'switch',
+};
+
+function App() {
+  return (
+    <React.Fragment>
+      {Object.keys(country).map((nature, idx) => {
+        const { text, size } = country[nature];
+        const obj = Object.assign({}, {name: nature, className: `${size}-flag`, text });
+
+        return (
+          <div key={idx}>
+            <h2>{`${size[0].toUpperCase()}${size.slice(1)}`} Flag Image Button</h2>
+            <ImageButton {...obj} />
+          </div>
+        );
+      })}
+      <div>
+        <h2>Switch Image Button</h2>
+        <ImageButton {...switchButton} />
+      </div>
+    </React.Fragment>
+  );
+}
+
+export default React.memo(App);
