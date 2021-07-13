90544aad4d2c05c96cc64267da37faa57a9f155e
Hayeon Kim <hayeon17kim@gmail.com>
2021-07-13 22:50:15 +0900

    refactor

--- src/App.js
+++ src/App.js
@@ -1,57 +1,34 @@
-import './App.css';
 import React from 'react';
-import ImageButton from './ImageButton.js';
-
-const korea = {
-  name: 'south-korea',
-  className: 'big-flag',
-  text: '대한민국 KRW',
-};
-
-const japan = {
-  name: 'japan',
-  className: 'big-flag',
-  text: '일본 JPY',
-};
-
-const unitedStates = {
-  name: 'united-states',
-  className: 'small-flag',
-  text: '미국 USD',
-};
-
-const mexico = {
-  name: 'mexico',
-  className: 'small-flag',
-  text: '멕시코 MXN',
-};
-
-const switchButton = {
-  name: 'switch',
-  className: 'switch',
-};
-
+import './App.css';
+import ImageButton from './components/ImageButton';
+import Input from './components/Input';
+import styled from 'styled-components';
+import country from './assets/dummy.json';
 
 function App() {
+  const InlineBlock = styled.div`
+    display: inline-block;
+  `;
 
   return (
-    <>
-      <div>
-        <h2>Big Flag Image Button</h2>
-        <ImageButton {...korea} />
-        <ImageButton {...japan} />
-      </div>
-      <div>
-        <h2>Small Flag Image Button</h2>
-        <ImageButton {...unitedStates} />
-        <ImageButton {...mexico}/>
-      </div>
-      <div>
-        <h2>Switch Image Button</h2>
-        <ImageButton {...switchButton} />
-      </div>
-    </>
+    <React.Fragment>
+      <h3>Input</h3>
+      <InlineBlock>
+        <ImageButton imageName="south-korea" />
+        <Input />
+      </InlineBlock>
+      <ImageButton imageName="switch" width="3rem" />
+      <InlineBlock>
+        <ImageButton imageName="united-states" />
+        <Input />
+      </InlineBlock>
+
+      <h3>Flag List</h3>
+      {country.map(({ imageName, title }, idx) => {
+        return <ImageButton key={idx} imageName={imageName} title={title} />;
+      })}
+    </React.Fragment>
   );
 }
 
-export default App;
+export default React.memo(App);
