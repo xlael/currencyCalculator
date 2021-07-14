d33140a5d4804499ceea71991e25d4863e9a7aa2
Hayeon Kim <hayeon17kim@gmail.com>
2021-07-14 21:29:21 +0900

    edit : app component

--- src/App.js
+++ src/App.js
@@ -1,32 +1,11 @@
 import React from 'react';
 import './App.css';
-import ImageButton from './components/ImageButton';
-import Input from './components/Input';
-import styled from 'styled-components';
-import country from './assets/dummy.json';
+import Layout from './components/Layout';
 
 function App() {
-  const InlineBlock = styled.div`
-    display: inline-block;
-  `;
-
   return (
     <React.Fragment>
-      <h3>Input</h3>
-      <InlineBlock>
-        <ImageButton imageName="south-korea" />
-        <Input />
-      </InlineBlock>
-      <ImageButton imageName="switch" width="3rem" />
-      <InlineBlock>
-        <ImageButton imageName="united-states" />
-        <Input />
-      </InlineBlock>
-
-      <h3>Flag List</h3>
-      {country.map(({ imageName, title }, idx) => {
-        return <ImageButton key={idx} imageName={imageName} title={title} />;
-      })}
+      <Layout />
     </React.Fragment>
   );
 }
