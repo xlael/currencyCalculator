2274416e9ec63ca754a1c8d0bd69e286e535e111
reyoucat <re.you.cat@gmail.com>
2021-07-18 13:54:31 +0900

    [#7] Fix: 스타일드 컴포넌트에서 문제되는 부분 수정

--- src/components/Popup.js
+++ src/components/Popup.js
@@ -1,25 +1,16 @@
 import React from 'react';
 import country from '../assets/dummy.json';
 import ImageButton from './ImageButton';
-import styled from 'styled-components';
+import { 
+  PopupArea, 
+  TopArticle, 
+  CloseButton
+} from '../styles';
+
+// TODO: 검색창이 들어갈 수 있는 영역 적용.
+// TODO: 온 클릭시 Layout의 상태를 prop 받아 닫고, x 버튼을 클릭하면 창을 안보이게 하는걸로 토글
 
 const Popup = () => {
-  // TODO: 이미지 버튼 컴포넌트를 이용해서 x 부분을 공통으로 변경.
-  // 검색창이 들어갈 수 있는 영역 적용.
-  // FIXME: 스타일드 컴포넌트 문제가 있는것으로 보임. 한곳으로 모을 것.
-  const PopupArea = styled.section`
-    padding: 10px;
-  `;
-
-  const TopArticle = styled.div`
-    display: flex;
-    justify-content: space-between;
-  `;
-
-  const CloseButton = styled.div`
-    width: 3rem;
-  `;
-
 
   return (
     <PopupArea>
